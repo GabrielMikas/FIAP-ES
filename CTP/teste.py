49d1aea9d5996ba2fcaf232fa43051fafaf6cc5d
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2023-03-01 13:31:53 -0300

    brabo

--- CTP/teste.py
+++ CTP/teste.py
@@ -1,3 +1,4 @@
-num = int(input("Digite um numero"))
-dobro = num + num
-print(dobro)
+nota1=5.5
+nota2=4.5
+meda=nota1+nota2/2
+print("a média das notas {0} e {1} = {2}".format(nota1, nota2, meda))
