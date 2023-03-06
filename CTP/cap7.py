100bb7c7983ce30962b51bd95e89b32fc0e10b79
GabrielMikas <112135725+GabrielMikas@users.noreply.github.com>
2023-03-06 16:35:44 -0300

    centralized

--- CTP/cap7.py
+++ CTP/cap7.py
@@ -3,9 +3,11 @@
 tempo_fumando = float(input("Tempo como fumante:"))
 valor_maco = float(input("Valor do maço:"))
 quantidade_diaria = float(input("Quantidade de maços por dia:"))
-tempo_fumando_meses = tempo_fumando
-tempo_fumando_meses = tempo_fumando * 360
-montante = tempo_fumando_meses * valor_maco * quantidade_diaria
+
+tempo_fumando_dias = tempo_fumando
+tempo_fumando_dias = tempo_fumando * 360
+montante = tempo_fumando_dias * valor_maco * quantidade_diaria
+
 if montante < 20000:
     print(f"Com o valor R${montante}, você poderia ter dado entrada em um carro.")
 elif montante >= 20000 and montante <= 50000:    
